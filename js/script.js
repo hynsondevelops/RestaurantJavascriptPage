4b2b81bc7fd50c09b72602a3c4844ef54076a3b4
Adam Hynson <hynsondevelops@gmail.com>
2017-07-21 17:30:13 -0400

    Updated Banner Buttons

--- js/script.js
+++ js/script.js
@@ -1,21 +1,34 @@
-$( document ).ready(function() {
- 
-    alert("hi")
-    $("#content").append("<ul> <li id = 'home'><input type = 'button'>Home</li> <li id = 'contact'><input id = 'contact' type = 'button'>Contact</li> <li><input type = 'button'>Menu</li> </ul>")
-    $("#content").append("<input type = 'button' onclick = ''>")
-    $("#content").append("<h3> Random Ingredients </h3>")
-    $("#content").append("<img src = 'src/HotDogCereal.jpg'>")
-    $("#content").append("<p> Welcome to Random Ingredients where your meal is just whatever happens to be in the kitchen that day bundled together and maybe microwaved. </p>")
-    $("#content").append("<h2> Menu </h2>")
+
+function loadContact() {
+	loadBanner()
+ 	$("#content").append("<p> Contact Information </p>")
+};
+
+function loadBanner() {
+	$("#content").empty()
+	$("#content").append("<table> <tr> <td id = 'home'><button type = 'button'>home</td> <td id = 'contact'><button id = 'contact' type = 'button'>Contact</td> <td><button id = 'menu' type = 'button'>Menu</td> </ul> </table>")
+	$("#content").append("<h3> Random Ingredients </h3>")
+	$("#home").click(loadHome)
+	$("#contact").click(loadContact)
+	$("#menu").click(loadMenu)
+}
+
+function loadMenu() {
+	loadBanner()
+	$("#content").append("<h2> Menu </h2>")
     $("#content").append("<p> $5 Special </p>")
     $("#content").append("<p> Three random ingredients from the kitchen in a bowl. Can be warm or cold based on customer preference </p>")
+}
+
+function loadHome() {
+	loadBanner()
+	$("#content").append("<img src = 'src/HotDogCereal.jpg'>")
+    $("#content").append("<p> Welcome to Random Ingredients where your meal is just whatever happens to be in the kitchen that day bundled together and maybe microwaved. </p>")
     $("#content").append("<h2> Customer Reviews </h2>")
     $("#content").append("<p> I got the $5 Special. The ingredients were a poptart, covered with jelly and a slice of pepperjack cheese. I couldn't even stomach it. Would not buy again!")
- 
+}
+
+$( document ).ready(function() {
+	loadHome()
 });
 
-
-
-$("#contact").click(function() {
-  $("#content").append("<p> Contact INFORMATION </p>")
-});
